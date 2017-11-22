3751b806a463a521cd186b0e7a166af6b5ebe3e2
Abhishek <abkanike@iu.edu>
2017-11-22 15:54:29 -0500

    Feature Extraction: Label
    Add label csv

--- sentiment_comment.py
+++ sentiment_comment.py
@@ -2,8 +2,12 @@ import pandas as pd
 import numpy as np
 import os
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pickle
+from textblob import TextBlob
+import xml.etree.ElementTree as ET
+from textstat.textstat import textstat
 
-SO_DATASET = os.path.join(os.path.dirname(__file__), 'data', 'dataset.csv.tar.gz')
+SO_DATASET = os.path.join(os.path.dirname(__file__), 'data', 'dataset.tar.gz')
 
 # Load Dataset
 def load_so_corpus(dataset):
@@ -11,18 +15,23 @@ def load_so_corpus(dataset):
 
 # Sentiment Analysis
 def analyzeCommentSentiment():
-	df = load_so_corpus(SO_DATASET)
-	# Sentiment Analyzer based on vader dataset
-	sid = SentimentIntensityAnalyzer()
-	# Top 10 comments polarity. Check ['compound'] property for polarity.
-	# -1 to 0 Negative polarity, 0 to +1 Positive polarity.
-	index = 0
-	for comment in range(index, index+10):
-		print ("\nComment:\n" + df["C_TEXT"][comment])
-		print (sid.polarity_scores(df["C_TEXT"][comment]))
+	# df = load_so_corpus(SO_DATASET)
+	# pickle.dump(df, open("saver_pickle", "wb"))
+	df = pickle.load(open("saver_pickle", "rb"))
 
+	answer_df = pd.DataFrame(columns = ["label"])
+	index = 0
+	for answer in range(index, df.shape[0]):
+		label = (1 if df["A_ID"][answer] == df["Accepted_Answer_ID"][answer] else 0)
+		# answer_df.append({"label": label})
+		print ("adding label at " + str(answer) + " label " + str(label))
+		answer_df.loc[answer] = [label]
+	# pd.write_csv(answer_df, )
+	answer_df.to_csv("answer.csv")
 def main():
 	analyzeCommentSentiment()
 
 if __name__ == '__main__':
 	main()
+
+
