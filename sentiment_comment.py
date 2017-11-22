5f609eee925e2081684dc3016aa2982bc33b9999
Abhishek <abkanike@iu.edu>
2017-11-21 20:21:29 -0500

    Initial Setup
    1. Data
    2. Comment polarity classification based on nltk vader data

---
+++ sentiment_comment.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import os
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+SO_DATASET = os.path.join(os.path.dirname(__file__), 'data', 'dataset.csv.tar.gz')
+
+# Load Dataset
+def load_so_corpus(dataset):
+	return pd.read_csv(dataset)
+
+# Sentiment Analysis
+def analyzeCommentSentiment():
+	df = load_so_corpus(SO_DATASET)
+	# Sentiment Analyzer based on vader dataset
+	sid = SentimentIntensityAnalyzer()
+	# Top 10 comments polarity. Check ['compound'] property for polarity.
+	# -1 to 0 Negative polarity, 0 to +1 Positive polarity.
+	index = 0
+	for comment in range(index, index+10):
+		print ("\nComment:\n" + df["C_TEXT"][comment])
+		print (sid.polarity_scores(df["C_TEXT"][comment]))
+
+def main():
+	analyzeCommentSentiment()
+
+if __name__ == '__main__':
+	main()
